b6a9ea234416716ef6b8b7e4123c2b401cc76a03
Pedro López <pedrolopez.e@hotmail.com>
2022-03-06 18:58:24 -0300

    Create 16x16 grid and hover event for first square

--- script.js
+++ script.js
@@ -1,7 +1,48 @@
 const container = document.querySelector(".container");
-const grid = document.createElement('div');
 
-for (let n = 0; n <= 16; n++){
-    container.appendChild(grid);
-    grid.classList.add("square");
-};
+function createSquares(row) {
+    for (let n = 0; n < 16; n++) {
+        row.appendChild(document.createElement('square'));
+    }
+}
+
+function changeBackgroundColor(event) {
+    event.target.style.backgroundColor = 'yellow';
+}
+
+for (let n = 0; n < 16; n++) {
+    let row = document.createElement('div');
+    container.appendChild(document.createElement('row'));
+}
+
+let square = document.createElement('div');
+const rows = document.querySelectorAll('row');
+const squares = document.querySelectorAll('square')
+
+rows.forEach(createSquares);
+
+square = document.querySelector('square');
+square.addEventListener('mouseenter', function(event) {
+    event.target.style.backgroundColor = 'yellow';
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
